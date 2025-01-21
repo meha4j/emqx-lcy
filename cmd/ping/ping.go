0e01e52473826ac31cdf9b3b960bac557ee25efd
Ivan Paraskun <ig.paraskun@gmail.com>
2025-01-21 13:54:15 +0700

    upd: latency estimation test

--- cmd/ping/ping.go
+++ cmd/ping/ping.go
@@ -1,45 +1,90 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 	"log/slog"
-	"math"
+	"net"
+	"strconv"
+	"sync"
 	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
 var (
-  dst string
-	top string
-	frq int
+	host string
+	src  int
+	dst  int
+	num  int
 )
 
+type event struct {
+	Stamp int64  `json:"stamp"`
+	Value string `json:"value"`
+}
+
 func init() {
-  flag.StringVar(&dst, "dst", "tcp://localhost:1883", "destination address")
-	flag.StringVar(&top, "top", "test", "topic to ping")
-	flag.IntVar(&frq, "frq", 1, "ping frequency")
+	flag.StringVar(&host, "h", "localhost", "")
+	flag.IntVar(&src, "src", 20041, "")
+	flag.IntVar(&dst, "dst", 1883, "")
+	flag.IntVar(&num, "num", 1000, "")
 }
 
 func main() {
-  flag.Parse()
-  slog.Info("pining", "addr", dst, "top", top, "freq", frq)
+	flag.Parse()
+	slog.Info("pinging", "host", host, "src", src, "dst", dst, "num", num)
 
-  cli := mqtt.NewClient(mqtt.NewClientOptions().AddBroker(dst))
+	cli := mqtt.NewClient(mqtt.NewClientOptions().
+		AddBroker(fmt.Sprintf("tcp://%s:%d", host, dst)),
+	)
 
-  if tok := cli.Connect(); tok.Wait() && tok.Error() != nil {
-    panic(tok.Error())
-  }
+	if tok := cli.Connect(); tok.Wait() && tok.Error() != nil {
+		log.Fatal(tok.Error())
+	}
 
-  tck := time.NewTicker(time.Duration(int(time.Second) / frq))
-  val := 0.0
+	est := 0.0
+	rec := 0
 
-  for {
-    t := <-tck.C
-    stamp := t.UnixMilli()
-    val += 0.1
+	wg := sync.WaitGroup{}
+	wg.Add(1)
 
-    cli.Publish(top, 0, false, fmt.Sprintf("{\"stamp\":%d,\"value\":%v}", stamp, math.Sin(float64(val))))
-  }
+	cli.Subscribe("/test/ping", 0, func(cli mqtt.Client, msg mqtt.Message) {
+		r := time.Now().UnixNano()
+		e := event{}
+
+		json.Unmarshal(msg.Payload(), &e)
+
+		m, _ := strconv.ParseInt(e.Value, 10, 64)
+		est += float64(float64(r-m) / float64(num))
+		rec += 1
+
+		if rec == num {
+			wg.Done()
+		}
+	})
+
+	con, err := net.Dial("tcp", fmt.Sprintf("%s:%d", host, src))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; i < num; i++ {
+		t := time.Now().UnixNano()
+
+		if _, err := con.Write([]byte(fmt.Sprintf("name:/test/ping|method:set|val:%d\n", t))); err != nil {
+			log.Fatal(err)
+		}
+
+    fmt.Printf("\rtotal sent: %d", i + 1)
+    time.Sleep(10 * time.Millisecond)
+	}
+
+  print("\n")
+
+	wg.Wait()
+	fmt.Printf("estimated latency: %f ns.\n", est)
 }
